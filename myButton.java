08f96baac18135b2d68c315e3452443f3295fae2
Mervan Kaya <mckaya@kth.se>
2021-09-29 14:14:39 +0200

    Update myButton.java

--- myButton.java
+++ myButton.java
@@ -1,19 +1,16 @@
 package Button;
 import java.awt.Color;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 
-public class myButton extends JButton implements ActionListener {
+public class myButton extends JButton{
 
 	private static final long serialVersionUID = 1L;
 	
-	Color first_c;
-	Color second_c;
-	String first_s;
-	String second_s;
-	static JButton button;
+	public Color first_c;
+	public Color second_c;
+	public String first_s;
+	public String second_s;
+	public JButton button;
 	
 	
 	public myButton(Color c1, Color c2, String s1, String s2) {		
@@ -23,25 +20,20 @@ public class myButton extends JButton implements ActionListener {
 		first_s = s1;
 		second_s = s2;
 		
-		button = new JButton();
 		this.setText(first_s);
 		this.setBackground(first_c);
-		this.addActionListener(this);
 	}
-	private void toggleState() {
+	
+	public void toggleState() {
 		String label = this.getText();
-		if (label == first_s) {
-			this.setText(second_s);
-			this.setBackground(second_c);
+		if (label == this.first_s) {
+			this.setText(this.second_s);
+			this.setBackground(this.second_c);
 		} else {
-			this.setText(first_s);
-			this.setBackground(first_c);
+			this.setText(this.first_s);
+			this.setBackground(this.first_c);
+			}
 		}
 		
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		toggleState();
-	}
-}
