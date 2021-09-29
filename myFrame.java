faf494a35791396c78bd3d3a3b79dfddf43126c7
Mervan Kaya <mckaya@kth.se>
2021-09-29 14:14:16 +0200

    Update myFrame.java

--- myFrame.java
+++ myFrame.java
@@ -1,16 +1,49 @@
 package Frame;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JFrame;
+import Button.myButton;
 
-public class myFrame extends JFrame {
+public class myFrame extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
+	public myButton[] buttons;
+	
 	
 	public myFrame() {
 		super("Frame made by Mervan Kaya");
+	
+		setSize(600,600); //width x height
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLayout(new FlowLayout());	
+		
+	}
+	
+	public myFrame(myButton[] button_list) {
+		super("Frame made by Mervan Kaya");
+		
+		this.buttons = button_list;
+		
+		for (myButton button: buttons) {
+			button.addActionListener(this); 
+			add(button);
+		}
+		//https://stackoverflow.com/questions/47292516/when-i-press-1-button-i-want-all-other-buttons-to-change-states
+		
 		setSize(600,600); //width x height
 		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLayout(new FlowLayout());		
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	for (myButton button: this.buttons){
+	    if (button != e.getSource())
+	        button.toggleState();
+		}
 	}
 }
