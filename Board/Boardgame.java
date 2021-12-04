e51ce2709119d9c1c074cba00c2a5c04d27e18f0
Mervan Kaya <mckaya@kth.se>
2021-12-04 16:38:49 +0100

    Delete Board directory

--- Board/Boardgame.java
+++
@@ -1,7 +0,0 @@
-package Board;
-
-public interface Boardgame {
-	public boolean move(int i, int j); //ger true om draget gick bra, annars false 
-	public String getStatus(int i, int j); // returnera innehåll på ruta (i,j)     
-	public String getMessage(); // returnera OK (eller liknande) eller felmeddelande avseende senaste drag
-	}
