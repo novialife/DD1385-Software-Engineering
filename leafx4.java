249b952cb3ccd5c037fdf25b93c692bc741f1b9a
Samhar Alzghaier <75775611+SuperSam1995@users.noreply.github.com>
2022-05-23 19:51:34 +0200

    Update leafx4.java

--- leafx4.java
+++ leafx4.java
@@ -1,1 +1,39 @@
 
+package x4;
+
+import java.util.ArrayList;
+
+public class LeafX4 extends ComponentX4 {
+
+    private String name;
+    private double weight;
+    private String content;
+
+    LeafX4(String name, double weight, String content) {
+        this.name = name;
+        this.weight = weight;
+        this.content = content;
+    }
+
+    LeafX4(String name) {
+        this.name = name;
+
+    }
+
+    @Override
+    public String toString() {
+        return name;
+
+    }
+
+    @Override
+    public boolean hasChildren() {
+        return false;
+    }
+
+    @Override
+    public ArrayList<ComponentX4> getChildren() {
+        return null;
+    }
+
+}
