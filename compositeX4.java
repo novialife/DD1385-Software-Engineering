ca5ec4d14a28f5e4b3e5019e760ff46eb6b3ef68
Samhar Alzghaier <75775611+SuperSam1995@users.noreply.github.com>
2022-05-23 19:52:22 +0200

    Update compositeX4.java

--- compositeX4.java
+++ compositeX4.java
@@ -1,1 +1,115 @@
 
+package x4;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+import x4.ComponentX4;
+
+
+public class CompositeX4 extends ComponentX4 implements Iterable{
+
+    private String name;
+    private double weight;
+    private ArrayList<ComponentX4> children = new ArrayList<ComponentX4>();
+    private String content;
+
+
+    CompositeX4(String name, double weight, String content) {
+        this.name = name;
+        this.weight = weight;
+        this.content = content;
+    }
+
+    CompositeX4(String name) {
+        this.name = name;
+
+    }
+
+    @Override
+    public String toString() {
+        return name;
+
+    }
+
+    public void addChild(ComponentX4 child) {
+        children.add(child);
+    }
+
+    public void removeChild(ComponentX4 child) {
+        children.remove(child);
+    }
+
+    @Override
+    public ArrayList<ComponentX4> getChildren(){
+        return children;
+
+    }
+
+    @Override
+    public boolean hasChildren(){
+        return children.size() > 0;
+    }   
+
+    @Override
+    public Iterator iterator(){
+        LinkedList queue = new LinkedList();
+
+        for (ComponentX4 child : children ){
+            queue.add(child);
+        }
+
+        return new Iterator() {
+            @Override
+            public boolean hasNext() {
+                return !queue.isEmpty();
+            }
+
+            @Override
+            public Object next() {
+                ComponentX4 child = (ComponentX4) queue.pop();
+                if (child.hasChildren()){
+                    for (ComponentX4 child2 : child.getChildren()){
+                        queue.add(child2);
+                    }
+                } 
+                return child;
+            }
+        };
+        
+    }
+
+    public Iterator deepiIterator(){
+        LinkedList queue = new LinkedList();
+
+        for (ComponentX4 child : children ){
+            queue.add(child);
+        }
+
+        return new Iterator() {
+            @Override
+            public boolean hasNext() {
+                return !queue.isEmpty();
+            }
+
+            @Override
+            public Object next() {
+                ComponentX4 child = (ComponentX4) queue.pop();
+                if (child.hasChildren()){
+                    for (ComponentX4 child2 : child.getChildren()){
+                        queue.add(child2);
+                    }
+                } 
+                return child;
+            }
+        };
+        
+    }
+
+
+
+
+
+
+}
